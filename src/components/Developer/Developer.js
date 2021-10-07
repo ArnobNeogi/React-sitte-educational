d329868cde2abae6c713db02a3215e43e1992c47
Arnob Neogi <sagor15-10825@diu.edu.bd>
2021-10-07 15:10:46 +0600

    new b

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -9,7 +9,7 @@ const Developer = () => {
     return (
         <section className="col-lg-5 col-md-8 col-10 mx-auto" style={{ marginTop: 80, marginBottom: 100 }}>
             <img className="img-fluid" src={coding} alt="" />
-            <p className="mt-4">Hi! My name is Arnob Neogi and I am a Computer Science student at RMIT Australia. I used React Js and React Bootstrap to build this application. This is a simple course based app where users are meant to do courses by adding them to their class list. The Application is fully responsive for all devices and I also tried to implement some User Centred Design ideas to build an application with good UI/UX.</p>
+            <p className="mt-4">Hi! My name is Arnob Neogi and I am a Computer Science student.. I used React Js and React Bootstrap to build this application. This is a simple course based app where users are meant to do courses by adding them to their class list. The Application is fully responsive for all devices and I also tried to implement some User Centred Design ideas to build an application with good UI/UX.</p>
             <small className="text-center fw-bold">#SoftwareEnthusiast</small>
             <br />
 
