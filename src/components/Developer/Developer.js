791377a9ac98ef2afc77053921f4e20163b4fcfe
Arnob Neogi <sagor15-10825@diu.edu.bd>
2021-10-07 15:27:10 +0600

    repo

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -11,8 +11,14 @@ const Developer = () => {
             <img className="img-fluid" src={coding} alt="" />
             <p className="mt-4">Hi! My name is Arnob Neogi and I am a Computer Science student.. I used React Js and React Bootstrap to build this application. This is a simple course based app where users are meant to do courses by adding them to their class list. The Application is fully responsive for all devices and I also tried to implement some User Centred Design ideas to build an application with good UI/UX.</p>
             <small className="text-center fw-bold">#SoftwareEnthusiast</small>
+
             <br />
 
+            <a target="_blank" rel="noreferrer" href="https://github.com
+            " className="btn btn-outline-primary btn-lg mt-3">About me <FontAwesomeIcon icon={faCode} /></a>
+            
+
+    
             
 
         </section>
